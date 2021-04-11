c6ca8b38bcb85bbc19d7f9ed85162b6e76c21ab4
Brandon Shearin <brandon.shearin@ibm.com>
2021-04-11 18:47:49 -0400

    ugh

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,28 +1,10 @@
 import fetch from "isomorphic-unfetch";
 import auth0 from "../../lib/auth0";
-import Cors from "cors";
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ["GET", "HEAD", "POST"],
-});
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 
 export default async function session(req, res) {
-  await runMiddleware(req, req, cors);
+  console.log("hey look here")
   try {
+    console.log("hey look here again")
     const tokenCache = auth0.tokenCache(req, res);
     const { accessToken } = await tokenCache.getAccessToken();
     res.status(200).json({ accessToken });
