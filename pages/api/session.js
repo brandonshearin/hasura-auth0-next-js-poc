580adae1ae56fe60820f8efc26d030879923f8c9
Brandon Shearin <brandon.shearin@ibm.com>
2021-04-11 18:32:14 -0400

    adding cors to session.js

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,18 +1,31 @@
 import fetch from "isomorphic-unfetch";
-
 import auth0 from "../../lib/auth0";
+import Cors from "cors";
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function session(req, res) {
+  await runMiddleware(req, req, cors);
   try {
-    res
-      .status(200)
-      .json({
-        accessToken:
-          "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjFPVExUREhVX2VYbU42aDBEamZCeiJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiXSwieC1oYXN1cmEtdXNlci1pZCI6Imdvb2dsZS1vYXV0aDJ8MTExNTM0NjQ3Mzc0NDczMjEwMjExIn0sImlzcyI6Imh0dHBzOi8vZGV2LTRjcDk2dHo3LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExMTUzNDY0NzM3NDQ3MzIxMDIxMSIsImF1ZCI6WyJodHRwczovL2hhc3VyYS5pby9sZWFybiIsImh0dHBzOi8vZGV2LTRjcDk2dHo3LnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MTgxNzc4NTEsImV4cCI6MTYxODI2NDI1MSwiYXpwIjoienhtZDdNZ2NhdzZWSHZqZEtOUzU3eVlPcHRHbTlKSEQiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIn0.rxmH-WSJqBTAOuLD8hWhCuynNsOFyl6JsP9DLgS13gh2ua-yf6m3yX3QL6L8-SOc_8HOGUp-9Z-rO_YDom8mDymBYjgyTF0vPUgD0wKtxajI3XCUMMQBN7WHvSAzZY120OZS1VzWPkMlEcDnrXhhcMDo3w23gXmv9Szuxw_6XpfQiWP8q-QT4B66aBDoV5vgAVp_R_BNkHHr3ox8pW2HFP9JA1PCugcqbRNyYzN8Bap24EB5WgodsFg4BaPmMsp2OQYcuByMywZLD3xZKtrdJCpYijE7aEVcKPShyfW9pLMKVxyfaxSa0uuJN-1JMwTft3N8_pZSDIxyWkAd9yUeXQ",
-      });
-    // const tokenCache = auth0.tokenCache(req, res);
-    // const { accessToken } = await tokenCache.getAccessToken();
-    // res.status(200).json({ accessToken });
+    const tokenCache = auth0.tokenCache(req, res);
+    const { accessToken } = await tokenCache.getAccessToken();
+    res.status(200).json({ accessToken });
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).json({
